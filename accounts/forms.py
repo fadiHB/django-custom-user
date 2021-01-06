7abba9ff6cc034658c055b12746843fbc5e9adc1
fadiHB <eng.fadi.hb@gmail.com>
2021-01-07 00:15:31 +0200

    fix

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,10 +6,9 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ('first_name', 'last_name', 'email')
+        fields = ('username', 'first_name', 'last_name', 'email')
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = ('first_name', 'last_name', 'email')
-
+        fields = ('username', 'first_name', 'last_name', 'email')
