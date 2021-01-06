dba3305d47f4e65978c779094f89c8fbe7009e33
fadiHB <eng.fadi.hb@gmail.com>
2021-01-06 22:11:25 +0200

    part1

---
+++ accounts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class CustomUser(AbstractUser):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField(max_length=64)
+
+
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
